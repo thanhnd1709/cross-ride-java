583ce33df174188a1c544227af3c065fe94a47cb
thanhnd1709 <ndthanht36@gmail.com>
2019-01-09 22:45:52 +0700

    A

--- cross-ride-java/src/main/java/com/crossover/techtrial/controller/RideController.java
+++ cross-ride-java/src/main/java/com/crossover/techtrial/controller/RideController.java
@@ -3,7 +3,6 @@
  */
 package com.crossover.techtrial.controller;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,8 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.crossover.techtrial.dto.TopDriverDTO;
 import com.crossover.techtrial.exceptions.RideException;
-import com.crossover.techtrial.model.Person;
 import com.crossover.techtrial.model.Ride;
+import com.crossover.techtrial.service.PersonService;
 import com.crossover.techtrial.service.RideService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -40,11 +39,26 @@ public class RideController {
 
 	@Autowired
 	private RideService rideService;
+	
+	@Autowired
+	private PersonService personService;
 
 	@PostMapping(path = "/api/ride")
 	public ResponseEntity<Ride> createNewRide(@Valid @RequestBody Ride ride) {
+		// if start time after end time, throw exception
 		if (ride.getStartTime().isAfter(ride.getEndTime())) {
 			throw new RideException("Ride start time should before end time");
+		}
+		if (ride.getRider() == null && ride.getDriver() == null) {
+			throw new RideException("Must at least has rider or driver");
+		}
+		// if rider has not been registered before
+		if (ride.getRider() != null && ride.getRider().getId() != 0 && personService.findById(ride.getRider().getId()) == null) {
+			throw new RideException("Rider has not been registered");
+		}
+		// if driver has not been registered before
+		if (ride.getDriver() != null && ride.getDriver().getId() != 0 && personService.findById(ride.getDriver().getId()) == null) {
+			throw new RideException("Driver has not been registered");
 		}
 		rideService.save(ride);
 		log.info("Ride {} was registered successfully", ride);
@@ -74,87 +88,7 @@ public class RideController {
 	public ResponseEntity<List<TopDriverDTO>> getTopDriver(@RequestParam(value = "max", defaultValue = "5") Long count,
 			@RequestParam(value = "startTime", required = true) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime startTime,
 			@RequestParam(value = "endTime", required = true) @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime endTime) {
-
-		
 		List<TopDriverDTO> topDrivers = rideService.getTopRides(count, startTime, endTime);
-		
-		
-		/*
-		// start implementation
-		// get rideList
-		List<Ride> rideList = rideService.getAll();
-		// filter to driver list which satisfied the start time and end time
-		rideList = rideList.stream().filter(ride -> ride.getDriver() != null)
-				.filter(ride -> !startTime.isAfter(LocalDateTime.parse(ride.getStartTime())))
-				.filter(ride -> !endTime.isBefore(LocalDateTime.parse(ride.getEndTime()))).collect(Collectors.toList());
-
-		// Get person list
-		List<Person> personList = personService.getAll();
-		for (Person person : personList) {
-			// for each person, get personal ride list
-			List<Ride> personalRideList = rideList.stream().filter(ride -> ride.getDriver().getId() == person.getId())
-					.collect(Collectors.toList());
-			if (personalRideList.size() > 0) {
-				// convert to TopDriverDTO with each person
-				TopDriverDTO tempDTO = convertToDto(person, personalRideList);
-				topDrivers.add(tempDTO);
-			}
-		}*/
-		// Here I a little bit confuse about the TOP 5 expression.
-		// What do you mean by "TOP", base on he is fastest in one ride, or fastest in
-		// average, or max total distance?
-		// Therefore, i just return 5 (or count) top rows
-		return ResponseEntity.ok(topDrivers.stream().limit(count).collect(Collectors.toList()));
-		//return null;
-
+		return ResponseEntity.ok(topDrivers.stream().collect(Collectors.toList()));
 	}
-
-	/**
-	 * This function is used to create the TopDriverDTO from current person and his
-	 * rides
-	 * 
-	 * @param person
-	 *            current person
-	 * @param personalRideList
-	 *            ride list of this person
-	 * @return
-	 */
-	private TopDriverDTO convertToDto(Person person, List<Ride> personalRideList) {
-		// initiate the top driver DTO
-		TopDriverDTO topDriverDTO = new TopDriverDTO();
-		// set the name and email of this dto to the value of current person
-		topDriverDTO.setName(person.getName());
-		topDriverDTO.setEmail(person.getEmail());
-		// initiate other properties
-		double averageDistance = 0.0;
-		double totalDistance = 0.0;
-		Long totalRideDurationInSeconds = 0L;
-		Long maxRideDurationInSecods = 0L;
-		int count = personalRideList.size();
-		// for loop to calculate total distance, total ride duration and maxride
-		// duration in seconds
-		for (Ride ride : personalRideList) {
-			// get totalDistance
-			totalDistance += ride.getDistance();
-			// get total ride duration in seconds
-			totalRideDurationInSeconds += Duration
-					.between(ride.getStartTime(), ride.getEndTime())
-					.getSeconds();
-			// calculate the max ride duration in second
-			Long currentRideDuration = Duration
-					.between(ride.getStartTime(), ride.getEndTime())
-					.getSeconds();
-			if (maxRideDurationInSecods < currentRideDuration) {
-				maxRideDurationInSecods = currentRideDuration;
-			}
-		}
-		// calculate the average distance value
-		averageDistance = totalDistance / count;
-		// set attributes to topDriverDTO
-		topDriverDTO.setAverageDistance(averageDistance);
-		topDriverDTO.setTotalRideDurationInSeconds(totalRideDurationInSeconds);
-		topDriverDTO.setMaxRideDurationInSecods(maxRideDurationInSecods);
-		return topDriverDTO;
-	}
-
 }
